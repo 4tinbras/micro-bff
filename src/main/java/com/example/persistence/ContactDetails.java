148756c27c6a4215a3e5cb9eb2fe9170e613eb86
Kacper Przychodzki <kaetoupi@gmail.com>
2026-08-02 11:45:27 +0100

    Updated model

--- src/main/java/com/example/persistence/ContactDetails.java
+++ src/main/java/com/example/persistence/ContactDetails.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 @Table(name = "contact_details")
 public class ContactDetails {
 
@@ -24,4 +25,43 @@ public class ContactDetails {
     @Digits(integer = 15, fraction = 0)
     private String phoneNo;
 
+    public Long getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Long uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
 }
