b50e33f8ef4c282b30449bdeac214917eb61f2aa
Kacper Przychodzki <kaetoupi@gmail.com>
2026-08-03 11:08:44 +0100

    Added happy path tests and updated controller

--- src/main/java/com/example/contact/ContactController.java
+++ src/main/java/com/example/contact/ContactController.java
@@ -1,7 +1,16 @@
 package com.example.contact;
 
+import com.example.persistence.ContactDetails;
+import io.micronaut.context.annotation.Parameter;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 
 @Controller
@@ -15,19 +24,30 @@ public class ContactController {
 
     @Get(uri = "/contacts")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAll() {
-        return "Hello World";
+    public MutableHttpResponse<List<ContactDetails>> getAll() {
+        return HttpResponse.ok(contactService.findAll());
     }
 
     @Post(uri = "/contact")
     @Produces(MediaType.APPLICATION_JSON)
-    public String postContact() {
-        return "Hello World";
+    public MutableHttpResponse<ContactDetails> postContact(@Body ContactDetails contactDetails) throws URISyntaxException {
+        final ContactDetails foundDetails = contactService.findByEmail(contactDetails.getEmail()).orElse(null);
+        if (foundDetails != null) {
+            if (!contactService.validateRecordToSave(contactDetails, foundDetails)) {
+                return HttpResponse.unprocessableEntity();
+            }
+            contactDetails.setUuid(foundDetails.getUuid());
+        }
+
+        final ContactDetails savedContact = contactService.save(contactDetails);
+//        log.debug("Updated contact with id: {}", savedContact.getUuid());
+        return HttpResponse.created(new URI("/contacts/" + savedContact.getUuid().toString())).body(savedContact);
     }
 
-    @Delete(uri = "/contact")
+    @Status(HttpStatus.NO_CONTENT)
+    @Delete(uri = "/contact/{id}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String deleteContact() {
-        return "Hello World";
+    public void deleteContact(@Parameter(value = "id") @PathVariable("name") Long id) {
+        contactService.deleteById(id);
     }
 }
